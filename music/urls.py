ec04eaa429b10482d06817eef0dcc6657d384f5f
Ethan Gee <a02388131@usu.edu>
2023-05-28 21:27:26 -0600

    Quality of life improvements.

--- music/urls.py
+++ music/urls.py
@@ -4,8 +4,12 @@ from . import views
 urlpatterns = [
 	path('', views.homepage, name='music'),
     path('all_data', views.all_data, name='all_data'),
-    path('artist/<str:artist_name>/', views.artist, name='artist'),
-    path('album/<str:album_title>/', views.album, name='album'),
+    path('artists/', views.artist_list, name="artists_list"),
+    path('artists/<str:artist_name>/', views.artist, name='artist_profile'),
+    path('albums/', views.album_list, name='album_list'),
+    path('albums/<str:album_title>/', views.album, name='album'),
+    path('genres/', views.genre_list, name='genre_list'),
+    path('favorites/', views.favorites, name='favorites'),
     path('test', views.test, name='test'),
 ]
 
--- music/views.py
+++ music/views.py
@@ -2,30 +2,56 @@ from django.shortcuts import render, get_object_or_404
 from .models import Album, Artist, Genre
 # Create your views here.
 
+
 def homepage(request):
     latest_albums = Album.objects.order_by('-review_date')[:6]
-    
-    return render(request, 'music/homepage.html', { 'latest_albums': latest_albums})
+    latest_favorite = Album.objects.filter(
+        favorite='True').order_by('review_date')[0]
+    print('\n\n\n\n\n\n')
+    print(latest_favorite)
+    print('\n\n\n\n\n\n')
+
+    return render(request, 'music/homepage.html', {'favorite': latest_favorite,'latest_albums': latest_albums})
 
 
 def all_data(request):
     colors = ["red", "crimson", "firebrick", "tomato", "coral", "salmon", "indianred", "orange", "orangered", "darkorange", "chocolate", "sandybrown", "yellow", "gold", "goldenrod", "khaki", "palegoldenrod", "green", "lime", "limegreen",
-                  "forestgreen", "seagreen", "olive", "olivedrab", "blue", "dodgerblue", "royalblue", "steelblue", "skyblue", "lightblue", "indigo", "darkslateblue", "slateblue", "mediumslateblue", "violet", "purple", "magenta", "fuchsia", "darkviolet", "darkorchid"]
+              "forestgreen", "seagreen", "olive", "olivedrab", "blue", "dodgerblue", "royalblue", "steelblue", "skyblue", "lightblue", "indigo", "darkslateblue", "slateblue", "mediumslateblue", "violet", "purple", "magenta", "fuchsia", "darkviolet", "darkorchid"]
 
     albums = Album.objects.all()
     artists = Artist.objects.all()
     genres = Genre.objects.all()
     return render(request, 'music/all_data.html', {'albums': albums, 'artists': artists, 'genres': genres, 'colors': colors})
 
+
 def artist(request, artist_name):
-    artist =  get_object_or_404(Artist, name=artist_name)
+    artist = get_object_or_404(Artist, name=artist_name)
     return render(request, 'music/artist.html', {'artist': artist})
 
+
 def album(request, album_title):
-    album =  get_object_or_404(Album, title=album_title)
+    album = get_object_or_404(Album, title=album_title)
     return render(request, 'music/album.html', {'album': album})
 
 
 def test(request):
-    color_list = ["red", "crimson", "firebrick", "tomato", "coral", "salmon", "indianred", "orange", "orangered", "darkorange", "chocolate", "sandybrown", "yellow", "gold", "goldenrod", "khaki", "palegoldenrod", "green", "lime", "limegreen", "forestgreen", "seagreen", "olive", "olivedrab", "blue", "dodgerblue", "royalblue", "steelblue", "skyblue", "lightblue", "indigo", "darkslateblue", "slateblue", "mediumslateblue", "violet", "purple", "magenta", "fuchsia", "darkviolet", "darkorchid"]
+    color_list = ["red", "crimson", "firebrick", "tomato", "coral", "salmon", "indianred", "orange", "orangered", "darkorange", "chocolate", "sandybrown", "yellow", "gold", "goldenrod", "khaki", "palegoldenrod", "green", "lime", "limegreen",
+                  "forestgreen", "seagreen", "olive", "olivedrab", "blue", "dodgerblue", "royalblue", "steelblue", "skyblue", "lightblue", "indigo", "darkslateblue", "slateblue", "mediumslateblue", "violet", "purple", "magenta", "fuchsia", "darkviolet", "darkorchid"]
     return render(request, 'music/test.html', {'colors': color_list})
+
+def artist_list(request):
+    artists = Artist.objects.all()
+    return render(request, 'music/artist_list.html',{'artists': artists})
+
+def album_list(request):
+    albums = Album.objects.all()
+    return render(request, 'music/album_list.html',{'albums': albums})
+
+def genre_list(request):
+    genres = Genre.objects.all()
+    return render(request, 'music/genre_list.html',{'genres': genres})
+
+def favorites(request):
+    albums = Album.objects.filter(favorite='True')
+    artists = Artist.objects.filter(favorite='True')
+    return render(request, 'music/favorites.html', {'albums': albums, 'artists': artists})
